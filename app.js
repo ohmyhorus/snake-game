3531be65b0a9dae31e3840a5fab3f95021950093
Sasha <45601549+ohmyhorus@users.noreply.github.com>
2023-04-10 22:32:31 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const squares = document.querySelectorAll(".grid div");
+  const scoreDisplay = document.querySelector("span");
+  const startBtn = document.querySelector(".start");
+
+  const width = 10;
+  let currentIndex = 0;
+  let appleIndex = 0;
+});
