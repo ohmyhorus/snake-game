18cdb16f2e2eaabbfa4c39c1871274c826ab42ef
Sasha <45601549+ohmyhorus@users.noreply.github.com>
2023-04-11 19:52:20 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const squares = document.querySelectorAll(".grid div");
-  const scoreDisplay = document.querySelector("span");
-  const startBtn = document.querySelector(".start");
+// board
+var blockSize = 25;
+var rows = 20;
+var cols = 20;
+var board;
+var context;
 
-  const width = 10;
-  let currentIndex = 0;
-  let appleIndex = 0;
-});
+//snake head
+var snakeX = blockSize * 5;
+var snakeY = blockSize * 5;
+
+window.onload = function () {
+  board = document.getElementById("board");
+  board.height = rows * blockSize;
+  board.width = cols * blockSize;
+  context = board.getContext("2d"); // for drawing on the board
+
+  update();
+};
+
+function update() {
+  context.fillStyle = "black";
+  context.fillRect(0, 0, board.width, board.height);
+}
